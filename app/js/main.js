b6b5623211372ad00ead39fdf87110fc5609ff5f
Roman <56957871+d1s4@users.noreply.github.com>
2019-11-27 21:10:29 +0200

    added feautured products slider, some sections fix, added icons to fonts

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,16 @@
-$(function(){
- 
-    $('.menu__search-btn').click(function () {
-        $('.menu__search-box').toggleClass('opened')
-    });
-    
-    // $('.features__slider').slick({
-        
-    // });
+$(function() {
+  $(".menu__search-btn").click(function() {
+    $(".menu__search-box").toggleClass("opened");
+  });
 
+  $('.featured__slider').slick({
+    slidesToShow: 1,
+    prevArrow: '<button type="button" class="slick-prev slick-arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow"></button>',
+  });
+  $(".rate-yo").rateYo({
+    starWidth: "15px",
+    rating: 4.2,
+    readOnly: true,
+  });
 });
