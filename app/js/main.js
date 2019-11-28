f4a269d69eb6bf07dcc66ee4b4d82f798c96958d
Roman <56957871+d1s4@users.noreply.github.com>
2019-11-28 12:09:41 +0200

    added section newest

--- app/js/main.js
+++ app/js/main.js
@@ -8,14 +8,21 @@ $(function() {
     prevArrow: '<button type="button" class="slick-prev slick-arrow"></button>',
     nextArrow: '<button type="button" class="slick-next slick-arrow"></button>'
   });
-
+  //TODO: add spesial star/ downloaded already
   $(".rate-yo").rateYo({
     starWidth: "15px",
     rating: 4.5,
-    readOnly: true
+    readOnly: true,
+    normalFill: "transparent"
+  });
+  $(".rate-yo__mini").rateYo({
+    starWidth: "12px",
+    rating: 4.5,
+    readOnly: true,
+    normalFill: "transparent"
   });
 
-  var mixer = mixitup(".newest__inner");
+  var mixer = mixitup(".newest__inner", {});
 
   // ! all about title + filter for mixitup
   $(".newest__btn-box").click(function() {
