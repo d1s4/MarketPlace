99e16a2375877569ad0643167a992cdea850b317
Roman <56957871+d1s4@users.noreply.github.com>
2019-11-28 10:32:03 +0200

    added item in global.scss, added newest title with animation

--- app/js/main.js
+++ app/js/main.js
@@ -3,14 +3,25 @@ $(function() {
     $(".menu__search-box").toggleClass("opened");
   });
 
-  $('.featured__slider').slick({
+  $(".featured__slider").slick({
     slidesToShow: 1,
     prevArrow: '<button type="button" class="slick-prev slick-arrow"></button>',
-    nextArrow: '<button type="button" class="slick-next slick-arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow"></button>'
   });
+
   $(".rate-yo").rateYo({
     starWidth: "15px",
-    rating: 4.2,
-    readOnly: true,
+    rating: 4.5,
+    readOnly: true
+  });
+
+  var mixer = mixitup(".newest__inner");
+
+  // ! all about title + filter for mixitup
+  $(".newest__btn-box").click(function() {
+    $(".newest__filter-panel").toggleClass("active");
+  });
+  $(".newest__btn-box").click(function() {
+    $(".newest__title").toggleClass("active");
   });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,8 @@ gulp.task("js", function() {
   return gulp
     .src([
       "node_modules/slick-carousel/slick/slick.js",
-      "node_modules/rateyo/src/jquery.rateyo.js"
+      "node_modules/rateyo/src/jquery.rateyo.js",
+      "node_modules/mixitup/dist/mixitup.js"
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
